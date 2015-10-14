952a8140f13bf42099b1292bd215a4f65d27a19e
Christian Neal <c.neal91@gmail.com>
2015-10-14 15:44:33 -0500

    Define all objects in api/postscontroller and changed views/api/posts to .json.jbuilder index, show and create objects are most likely correct.  Edit, change, and delete need some more work

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,9 +1,33 @@
 class Api::PostsController < ApplicationController
+
+  before_action do
+    request.format = :json
+  end
+  
   def index
     @Posts = Post.all
   end
 
   def show
+    @Post = Post.find params[:id]
+  end
+
+  def create
+    @Post = Post.new.require(:post).permit(:title, :body)
+    if @Post.save
+      redirect_to '/api/posts'
+    end
+  end
+
+  def edit
+    @Post = Post.find params[:id]
+  end
+
+  def change
+    @Post = Post.find params[:id]
+  end
+
+  def delete
     @Post = Post.find params[:id]
   end
 end
